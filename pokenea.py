6f78b0a6dbff0438aa09dfacfebff066d74fab96
edup3 <eduardopineros@hotmail.com>
2025-05-06 18:32:33 -0500

    Finished Pokeneas DB

--- pokenea.py
+++ pokenea.py
@@ -6,16 +6,15 @@ import random
 app = Flask(__name__)
 
 pokeneas = [
-    "Greninja",
-    "Clauncher",
-    "Kingambit",
-    "Houndoom",
-    "Beedril",
-    "Zangoose",
-    "Lopunny",
-    "Buzzwhole",
-    "Rillaboom",
-    "WeezinYandel"
+    {'id':1,'name': "Neaninja",'ability':'Puñalada Multiple','phrase':'Su mamá sabe coser?','height':150},
+    {'id':2,'name': "Camareta",'ability':'Humo Espontaneo','phrase':'Camaron que se parcha se lo lleva la recocha','height':50},
+    {'id':3,'name': "Kingnea",'ability':'Atraco Instantaneo','phrase':'Hermano me regala la hora?','height':200},
+    {'id':4,'name': "Princesa",'ability':'Comer niños','phrase':'Perro que ladra no muerde','height':60},
+    {'id':5,'name': "Machampeta",'ability':'Los prohibidos','phrase':'Chawa Chawa!','height':1500},
+    {'id':6,'name': "JynxG",'ability':'Bichotear','phrase':'Que mas moooooor!','height':110},
+    {'id':7,'name': "OmareepCourtz",'ability':'Auto-tune Shock','phrase':'No todos los rayos iluminan, algunos solo te despiertan para que no te duermas en tu propio flow pah!','height':60},
+    {'id':8,'name': "Buzzchimbita",'ability':'Hipertrofia','phrase':'No pain no gain!','height':240},
+    {'id':9,'name': "WeezinYandel",'ability':'Perreo','phrase':'EL DUO DE LA HISTORIA!','height':120}
 ]
 
 
@@ -23,15 +22,17 @@ pokeneas = [
 def get_random_pokenea():
     pokenea = random.choice(pokeneas)
     container_id = os.uname()[1]
-    return jsonify(name=pokenea, container=container_id)
+    pokenea.update({'container': container_id})
+    return jsonify(id=pokenea.get('id'),name = pokenea.get('name'),height=pokenea.get('height'),ability=pokenea.get('ability'),container_id = container_id)
 
 
 @app.route('/show/pokenea')
 def show_random_pokenea():
     pokenea = random.choice(pokeneas)
+    frase = pokenea.get('phrase')
     container_id = os.uname()[1]
-    image_url = f"https://taller2bucketedup3.s3.us-east-1.amazonaws.com/pokeneas/{pokenea}.jpg"
-    return render_template('pokedex.html', image=image_url)
+    image_url = f"https://taller2bucketedup3.s3.us-east-1.amazonaws.com/pokeneas/{pokenea.get('name')}.jpg"
+    return render_template('pokedex.html', image=image_url, container=container_id, frase=frase)
 
 
 if __name__ == '__main__':
